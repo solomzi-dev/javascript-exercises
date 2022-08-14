5fc5c72abf2f06459b5de67023685129b17aa292
Solomzi <solomzi.dev@gmail.com>
2022-08-14 16:09:22 +0200

    finish removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const newArr = args.splice(0, 1)[0];
 
+    return newArr.filter(item => !args.includes(item))
 };
+
+console.log(removeFromArray([1,2,3,4], 2,4,5));
 
 // Do not edit below this line
 module.exports = removeFromArray;
