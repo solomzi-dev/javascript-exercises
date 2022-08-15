fe432ea8018bb44c1efe142d20832d919d7720bc
Solomzi <solomzi.dev@gmail.com>
2022-08-15 12:20:36 +0200

    finish sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    if(isNaN(a) || isNaN(b)) return 'ERROR';
+    if(typeof a === 'string' || typeof b === 'string') return 'ERROR';
+    if(a < 0 || b < 0) return 'ERROR';
+    if(a > b) {
+        let diff = a - b;
+        sum = a + b;
+        for (let i = 1; i < diff; i++) {
+            sum = sum + b + i;
+        }
+        return sum;
+    }  else if(b > a) {
+        let diff = b - a;
+        sum = a + b;
+        for (let i = 1; i < diff; i++) {
+            sum = sum + a + i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
