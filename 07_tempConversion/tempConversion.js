37ee65db30ada444a5e5b79b20b9822befb3e8fd
Solomzi <solomzi.dev@gmail.com>
2022-08-28 14:54:47 +0200

    Finish all exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  const tempCel = (temp - 32) * 5/9;
+  return Math.round(tempCel * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  const tempFeh = (temp * 9/5) + 32;
+  return Math.round(tempFeh * 10) / 10;
 };
 
 // Do not edit below this line
@@ -11,3 +13,9 @@ module.exports = {
   ftoc,
   ctof
 };
+
+
+console.log(ftoc(32));
+console.log(ftoc(100));
+console.log(ctof(0));
+console.log(ctof(73.2));
